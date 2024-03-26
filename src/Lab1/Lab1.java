2ee7a2969ef120d24df00ead05eb7ac566099543
Mob <43564622+Medrov@users.noreply.github.com>
2024-03-26 14:35:51 +0300

    Delete redundant classes.

--- src/Lab1/Lab1.java
+++ src/Lab1/Lab1.java
@@ -5,21 +5,6 @@ import java.util.List;
 
 public class Lab1 {
     public static void main(String[] args) {
-        UniversityLibraryFactory universityLibraryFactory = new UniversityLibraryFactory();
-        UserBuilder userBuilder = new UserBuilder();
-        List<UserBuilder> users = new ArrayList<>();
 
-        for (String userType : new String[]{"student", "lecturer"}) {
-            userBuilder.createUser(userType);
-            userBuilder.addBooks(universityLibraryFactory);
-            users.add(userBuilder);
-        }
-
-        for (UserBuilder user : users) {
-            System.out.println(user.getUserType() + ":");
-            for (Book book : user.getBooks()) {
-                System.out.println("- " + book);
-            }
-        }
     }
 }
--- src/Lab1/LibraryFactory.java
+++
@@ -1,6 +0,0 @@
-package Lab1;
-
-public interface LibraryFactory {
-    Book createTextbook(String title, String language);
-    Book createFictionBook(String title, String language);
-}
